b0a3bd5c4ab619bc30bf1a1ef07d83e6fb6ca5b5
nelson truran <nelson@truran.dev>
2024-02-23 13:40:24 -0800

    show sub data

--- app/youtube.py
+++ app/youtube.py
@@ -5,15 +5,17 @@ from app.config import settings
 async def resubscribe(topic: str):
     print(f"Resubscribing to {topic}")
     async with aiohttp.ClientSession() as session:
+        data = {
+            "hub.callback": f"{settings.base_url}/youtube/hook",
+            "hub.mode": "subscribe",
+            "hub.topic": topic,
+            "hub.verify": "async",
+            "hub.verify_token": settings.youtube_verify_token,
+        }
+        print(data)
         async with session.post(
             "https://pubsubhubbub.appspot.com/subscribe",
-            data={
-                "hub.callback": f"{settings.base_url}/youtube/hook",
-                "hub.mode": "subscribe",
-                "hub.topic": topic,
-                "hub.verify": "async",
-                "hub.verify_token": settings.youtube_verify_token,
-            },
+            data=data,
         ) as resp:
             if resp.ok:
                 print(f"Resubscribed to {topic}")
